431b3fbc00f425b02362d5c220f2efab1ec40fea
Ferry <eplious.fan@gmail.com>
2016-01-27 16:36:31 +0800

    Update 05.scala

--- Chapter14/05.scala
+++ Chapter14/05.scala
@@ -2,7 +2,7 @@
 def sumLeaf(list: List[Any]) : Int = {
 	(for(elem <- list) yield ( 
 		elem match {
-			case x: List[Int] => sumLeaf(x)
+			case x: List[Int] => sumLeaf(x)   //Generic information is erased during runtime, also don't match List(List(1,2),3) .Should be List[_]
 			case x: Int => x
 			case _ => 0
 		}
